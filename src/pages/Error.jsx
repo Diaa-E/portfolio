3fcaa6e157aef7c9bbec3d73f442c581730e5c68
Diaa-E <blarb21@yahoo.com>
2024-07-18 16:56:54 +0300

    Show error details on error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,6 +1,19 @@
+import { useRouteError, Link } from "react-router-dom";
+
 export default function Error({ })
 {
+    const error = useRouteError();
+    console.error(`${error.status}: ${error.statusText}`);
+    
     return (
-        <h1>Error</h1>
+        <>
+            <h1>Error</h1>
+            <h2>{error.status}: {error.statusText}</h2>
+            <ul>
+                <li>
+                    <Link to={"/"}>Home</Link>
+                </li>
+            </ul>
+        </>
     )
 }

3f970eba5810de1f2f06e023e18f6cacc65265ee
Diaa-E <blarb21@yahoo.com>
2024-07-18 20:13:48 +0300

    Add nav bar to portfolio pages

--- src/pages/ArtPortfolio.jsx
+++ src/pages/ArtPortfolio.jsx
@@ -1,8 +1,12 @@
+import NavBar from "../components/NavBar";
 import styles from "./ArtPortfolio.module.css";
 
 export default function ArtPortfolio({ })
 {
     return (
-        <h1>Art</h1>
+        <>
+            <NavBar />
+            <h1>Art</h1>
+        </>
     )
 }
--- src/pages/WebPortfolio.jsx
+++ src/pages/WebPortfolio.jsx
@@ -1,8 +1,27 @@
+import NavBar from "../components/NavBar";
 import styles from "./WebPortfolio.module.css";
 
 export default function WebPortfolio({ })
 {
     return (
-        <h1>Web</h1>
+        <>
+            <NavBar
+                scrollAnchors={[
+                    {
+                        to: "#about",
+                        text: "About Me"
+                    },
+                    {
+                        to: "#work",
+                        text: "My Work"
+                    },
+                    {
+                        to: "#skills",
+                        text: "My Skills"
+                    }
+                ]}
+            />
+            <h1>Web</h1>
+        </>
     )
 }

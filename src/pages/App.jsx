931fd0009105d7b6dce0709faef98b369340b0c6
Diaa-E <blarb21@yahoo.com>
2024-07-18 16:52:47 +0300

    Add nav links for each portfolio

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,9 +1,18 @@
 import './App.css';
 
+import { Link } from 'react-router-dom';
+
 function App() {
 
   return (
-    <h1>app</h1>
+    <ul>
+      <li>
+        <Link to={"web"}>Web Portfolio</Link>
+      </li>
+      <li>
+        <Link to={"art"}>Art Portfolio</Link>
+      </li>
+    </ul>
   )
 }
 

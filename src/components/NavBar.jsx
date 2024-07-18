0b02f236daa7a6954223183dd04790b4ad936e7c
Diaa-E <blarb21@yahoo.com>
2024-07-18 21:49:59 +0300

    Add basic styling to nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,11 @@
+import styles from "./NavBar.module.css";
 import { Link } from "react-router-dom";
 
 export default function NavBar({ scrollAnchors = [{to: "#root", text: "root"}] })
 {
     return (
-        <nav>
-            <ul>
+        <nav className={styles["nav-bar"]}>
+            <ul className={styles["link-list"]}>
             {
                 scrollAnchors.map(anchor => (
                     <li key={anchor.to}>
@@ -13,7 +14,7 @@ export default function NavBar({ scrollAnchors = [{to: "#root", text: "root"}] }
                 ))
             }
             </ul>
-            <ul>
+            <ul className={styles["link-list"]}>
                 <li>
                     <Link to={"/"}>Home</Link>
                 </li>

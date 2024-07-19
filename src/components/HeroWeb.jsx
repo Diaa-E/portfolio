8d44a09249f1d3e8cb7e00556648a539eff56a29
Diaa-E <blarb21@yahoo.com>
2024-07-19 19:58:35 +0300

    Add styles to web portfolio hero

--- src/components/HeroWeb.jsx
+++ src/components/HeroWeb.jsx
@@ -3,12 +3,15 @@ import styles from "./HeroWeb.module.css";
 export default function HeroWeb({ })
 {
     return (
-        <div id="#about">
+        <div id="#about" className={styles["hero"]}>
             <h1 className={styles["title"]}>
-                <span>Hi, I'm</span>
-                <span>Diaa</span>
-                <span>A</span>
-                <span>Front End Web Developer</span>
+                <span className={styles["graffiti-one"]}>Hi, I'm</span>
+                <br />
+                <span className={styles["neon-one"]}>Diaa</span>
+                <br />
+                <span className={styles["graffiti-two"]}>A</span>
+                <br />
+                <span className={styles["neon-two"]}>Front End <br /> Web Developer</span>
             </h1>
         </div>
     )

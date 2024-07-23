8a210e82eda0c9850b04394dcb28f635810fa452
Diaa-E <blarb21@yahoo.com>
2024-07-23 23:44:08 +0300

    Remove all break tags to fix varying line height on different browsers

--- src/components/HeroWeb.jsx
+++ src/components/HeroWeb.jsx
@@ -6,11 +6,8 @@ export default function HeroWeb({ })
         <div id="about" className={styles["hero"]}>
             <h1 className={styles["title"]}>
                 <span className={styles["graffiti-one"]}>Hi, I'm</span>
-                <br />
                 <span className={styles["neon-one"]}>Diaa</span>
-                <br />
                 <span className={styles["graffiti-two"]}>A</span>
-                <br />
                 <span className={styles["neon-two"]}>Front End <br /> Web Developer</span>
             </h1>
         </div>

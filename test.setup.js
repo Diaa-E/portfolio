c027bcc35dbda8298e6dca6f7a4fe9e4a3a45703
Diaa-E <blarb21@yahoo.com>
2024-08-11 13:40:09 +0300

    Mock intersection observer API

--- test.setup.js
+++ test.setup.js
@@ -1,4 +1,4 @@
-import { expect, afterEach } from 'vitest';
+import { expect, afterEach, vi } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import * as matchers from "@testing-library/jest-dom/matchers";
 
@@ -7,3 +7,9 @@ expect.extend(matchers);
 afterEach(() => {
   cleanup();
 });
+
+//mock intersection observer
+global.IntersectionObserver = vi.fn().mockReturnValue({
+  observe: () => null,
+  disconnect: () => null
+})

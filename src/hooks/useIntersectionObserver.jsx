e413df1f6572a7d7cea4294d1603c5fef3ab2284
Diaa-E <blarb21@yahoo.com>
2024-08-11 14:04:39 +0300

    Fix negation

--- src/hooks/useIntersectionObserver.jsx
+++ src/hooks/useIntersectionObserver.jsx
@@ -5,7 +5,7 @@ export default function useIntersectionObserver(refsArray, intersectionCallback,
     useEffect(() => {
 
         //fallback if intersection observer isn't supported
-        if (!"IntersectionObserver" in window)
+        if (!("IntersectionObserver" in window))
         {
             fallbackCallback();
 

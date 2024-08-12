98926bffc42021dc70bc235a4d2a07454bcf88e8
Diaa-E <blarb21@yahoo.com>
2024-08-12 15:13:35 +0300

    Add animation to skills section

--- src/components/SkillItem.jsx
+++ src/components/SkillItem.jsx
@@ -1,18 +1,33 @@
+import { useRef } from "react";
+import { generateNeonTextActivator, generateNeonTextInlineStyle } from "../utils/neonTextUtils";
 import styles from "./SkillItem.module.css";
 import PropTypes from "prop-types";
+import useIntersectionObserver from "../hooks/useIntersectionObserver";
 
 export default function SkillItem({ name, color, tools = [] })
 {
+    const itemRef = useRef(null);
+    const titleRef = useRef(null);
+
+    useIntersectionObserver(
+        [
+            itemRef,
+            titleRef
+        ],
+        generateNeonTextActivator(styles["active"]),
+        () => {
+            titleRef.current.classList.add(styles["active"]);
+            itemRef.current.classList.add(styles["active"]);
+        }
+    );
+
     return (
         <div
             className={styles["tools-wrapper"]}
-            style={{
-                "--glow": `var(--glow-single-${color})`,
-                "--color": `var(--neon-${color})`,
-            }}
+            style={generateNeonTextInlineStyle(color, Math.random() * .7, Math.ceil(Math.random() * 2) + 1)}
         >
-            <h3 className={styles["skill-name"]}>{name}</h3>
-            <ul className={styles["tools-list"]}>
+            <h3 ref={titleRef} className={styles["skill-name"]}>{name}</h3>
+            <ul  ref={itemRef} className={styles["tools-list"]}>
             {
                 tools.map(tool => {
                     return <li className={styles["tool-name"]} key={tool}>

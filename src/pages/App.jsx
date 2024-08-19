08d1c5836986abf44d4587bb492981ac863f17e7
Diaa-E <blarb21@yahoo.com>
2024-08-19 18:08:42 +0300

    Add random colors to portolio links

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,16 +1,29 @@
+import { generateRandomNeonColor } from '../utils/neonTextUtils';
 import styles from './App.module.css';
 
 import { Link } from 'react-router-dom';
 
 function App() {
 
+  const color1 = generateRandomNeonColor();
+  const color2 = generateRandomNeonColor();
   return (
     <div className={styles["topic-background"]}>
       <ul className={styles["links-wrapper"]}>
-        <li>
+        <li style={{
+          "--off": `var(--neon-${color1}-off)`,
+          "--on": `var(--neon-${color1})`,
+          "--backlight": `var(--backlight-${color1})`,
+          "--glow": `var(--glow-single-${color1})`,
+        }}>
           <Link className={styles["web-link"]} to={"web"}>Web Dev</Link>
         </li>
-        <li>
+        <li style={{
+          "--off": `var(--neon-${color2}-off)`,
+          "--on": `var(--neon-${color2})`,
+          "--backlight": `var(--backlight-${color2})`,
+          "--glow": `var(--glow-single-${color2})`,
+        }}>
           <Link className={styles["art-link"]} to={"art"}>3D Art</Link>
         </li>
       </ul>
